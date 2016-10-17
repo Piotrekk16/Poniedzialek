7bc84acc6e0d8175a6935b3235c7799f9da762e1
Piotrek <piotr.jastr@gmail.com>
2016-10-17 13:00:12 +0200

    dodanie plików projektu

---
+++ ConsoleApplication14/Car.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication14
+{
+    class Car
+    {
+        private double pojemnoscSilnika;
+        private string marka;
+        
+        public Car()
+        {
+            this.marka = "Skoda";
+            this.pojemnoscSilnika = 5.1;
+        }
+        public Car (double pojemnoscSilnika, string marka)
+        {
+            this.marka = marka;
+            this.pojemnoscSilnika = pojemnoscSilnika;
+        }
+        static public Car Create()
+        {
+            return new Car();
+        }
+        static int iloscKol;
+        static Car()
+        {
+            iloscKol = 4;
+        }
+        
+        
+    }
+}
---
+++ ConsoleApplication14/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car();
+            Car car2 = new Car(4.8, "Ford");
+
+            Car car3 = Car.Create();
+            Console.ReadKey();
+            
+        }
+    }
+}
